a4b68fe67179243cf270d970f462ca337e042477
cybrix-dev <cybrix@gmx.de>
2020-04-27 14:59:32 +0200

    remove mouse-cursor

--- __main__.py
+++ __main__.py
@@ -1,10 +1,15 @@
 import sys
 
 from PyQt5.Qt import QApplication
+from PyQt5.QtCore import Qt
 from box import MainWindowB, Box
 
 
 app = QApplication(sys.argv)
+
+# disable mouse-cursor
+app.setOverrideCursor(Qt.BlankCursor)
+
 MainWindow = MainWindowB()
 test = Box(MainWindow)
 MainWindow.show()
