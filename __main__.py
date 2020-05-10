b45393cbcf2bd8b0b27600c93d2f21b8176581d5
cybrix-dev <cybrix@gmx.de>
2020-05-10 15:06:18 +0200

    reduce timeout for detecting immediate crash to 3s

--- __main__.py
+++ __main__.py
@@ -12,7 +12,7 @@ while res != 0:
     except:
         res = -1
     stop_time = time.time()
-    if (res != 0) and (stop_time < (start_time + 5)):
+    if (res != 0) and (stop_time < (start_time + 3)):
         # serious bug, we just can just stop the application
         res = 0
         print("Fast error. Try calling box.py directly for more information")
